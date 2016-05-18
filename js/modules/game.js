bcf1a4d90408c7b2fa770eb1cc391e330e1cbbfe
Ryan McGlinn <rgm9922@rit.edu>
2016-05-18 12:52:37 -0400

    Add Actor class object
    Actors will represent interactable elements within the
    scenePhase space.

--- js/modules/game.js
+++ js/modules/game.js
@@ -33,8 +33,8 @@ function game(){
     draggingDisabled = false;
     mouseSustainedDown = false;
     
-    activePhase = new BoardPhase("https://atlas-backend.herokuapp.com/repos");
-    //activePhase = new ScenePhase("testScene05", 0);
+    //activePhase = new BoardPhase("https://atlas-backend.herokuapp.com/repos");
+    activePhase = new ScenePhase("testScene05", 0);
 }
 
 var p = game.prototype;
