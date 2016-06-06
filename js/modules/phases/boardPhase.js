8d8bc394464345e05426c319c4f509f31cd7ba20
Ryan McGlinn <rgm9922@rit.edu>
2016-06-06 17:49:01 -0400

    Stagger columns of lessonNodes based on height
    Columns of lessonNode elements are staggered vertically
    based on their height to ensure even spacing.

--- js/modules/phases/boardPhase.js
+++ js/modules/phases/boardPhase.js
@@ -111,7 +111,7 @@ function boardLoadedCallback(pJSONElements){
         var subArray = nodeArray[i];
         for(var j = 0; j < subArray.length; j++){
             //assign position values
-            nodeArray[i][j].position = new Point(i * 280, j * 280);
+            nodeArray[i][j].position = new Point(i * 280, j * 280 - (((subArray.length - 1) * 280) / 2));
         }
     }
     
