4047f215efcb6b64e631463b75234e85e1bccc5a
Ryan McGlinn <rgm9922@rit.edu>
2016-06-09 12:13:09 -0400

    Refactor utilities.js
    utilities.js set to use updated public method prototype format

--- js/modules/utilities.js
+++ js/modules/utilities.js
@@ -4,21 +4,20 @@ var Point = require('./point.js');
 function utilities(){
 }
 
-var p = utilities.prototype;
 // returns mouse position in local coordinate system of element
-p.getMouse = function(e){
+utilities.prototype.getMouse = function(e){
     return new Point((e.pageX - e.target.offsetLeft), (e.pageY - e.target.offsetTop));
 }
 
-p.map = function(value, min1, max1, min2, max2){
+utilities.prototype.map = function(value, min1, max1, min2, max2){
     return min2 + (max2 - min2) * ((value - min1) / (max1 - min1));
 }
 
-p.clamp = function(value, min, max){
+utilities.prototype.clamp = function(value, min, max){
     return Math.max(min, Math.min(max, value));
 }
 
-p.mouseIntersect = function(pMouseState, pElement, pOffsetter, pScale){
+utilities.prototype.mouseIntersect = function(pMouseState, pElement, pOffsetter, pScale){
     if(pMouseState.relativePosition.x + pOffsetter.x > (pElement.position.x - (pScale*pElement.width)/2) && pMouseState.relativePosition.x + pOffsetter.x < (pElement.position.x + (pScale*pElement.width)/2)){
         if(pMouseState.relativePosition.y + pOffsetter.y > (pElement.position.y - (pScale*pElement.height)/2) && pMouseState.relativePosition.y + pOffsetter.y < (pElement.position.y + (pScale*pElement.height)/2)){
             pElement.mouseOver = true;
