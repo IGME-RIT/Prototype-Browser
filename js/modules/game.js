de0f64ab8f7fcaa4a7b9230690d687ef2e6c3813
Ryan McGlinn <rgm9922@rit.edu>
2016-07-28 18:58:29 -0400

    Streamline game-level draw method
    All draw parameters have been replaced with a single canvasState
    variable.

--- js/modules/game.js
+++ js/modules/game.js
@@ -1,89 +1,47 @@
 "use strict";
-var BoardPhase = require('./phases/boardPhase.js');
-
-var Point = require('./point.js');
+//imported objects
+var BoardPhase = require('./phases/BoardPhase.js');
 var DrawLib = require('./drawLib.js');
-var LessonNode = require('./lessonNode.js');
 var Utilities = require('./utilities.js');
 
-var GAME_PHASE = Object.freeze({LANDING: 0, SELECTION: 1, BOARD: 2});
-
-var painter;
-var board;
-var utility;
-var phaseEnum;
 var activePhase;
+var painter;
+var utility;
 
 var mouseState;
 var previousMouseState;
-var draggingDisabled;
-var mouseTarget;
-var mouseSustainedDown;
 
-var JSONLoad = false;
-
-var progressData;
-
-
-function game(){    
+function Game(){    
     painter = new DrawLib();
     utility = new Utilities();
-    phaseEnum = GAME_PHASE.BOARD;
     
-    draggingDisabled = false;
-    mouseSustainedDown = false;
-    
+    //instantiate a phase, phases have universal function calls and callable variables
     activePhase = new BoardPhase("https://atlas-backend.herokuapp.com/repos");
     
-    
-    
-    //activePhase = new ScenePhase("testScene05", 0);
+    //give mouseState a value from the start so it doesn't pass undefined to previous
+    mouseState = 0;
 }
-
-var p = game.prototype;
 
 //passing context, canvas, delta time, center point, usable height, mouse state
-p.update = function(ctx, canvas, dt, center, activeHeight, pMouseState){
+Game.prototype.update = function(ctx, canvas, dt, center, activeHeight, pMouseState, canvasState){
     previousMouseState = mouseState;
     mouseState = pMouseState;
-    mouseTarget = 0;
     
+    //game class specific draw calls
+    this.draw(canvasState);
     
-    
-    if(typeof previousMouseState === 'undefined'){
-        previousMouseState = mouseState;
-    }
-    
-    
-    if(activePhase === GAME_PHASE.BOARD){
-        //update active board object
-    }
-    //update stuff
-    p.act();
-    //draw stuff
-    p.draw(ctx, canvas, center, activeHeight);
-    
-    activePhase.update(ctx, canvas, dt, center, activeHeight, pMouseState);
+    //update key variables in the active phase
+    activePhase.update(ctx, canvas, dt, center, activeHeight, pMouseState, canvasState);
 }
 
-p.act = function(){
-    
-    
-    
-}
-
-p.draw = function(ctx, canvas, center, activeHeight){
+Game.prototype.draw = function(canvasState){
     //draw board
-    ctx.save();
-    painter.clear(ctx, 0, 0, canvas.offsetWidth, canvas.offsetHeight);
-    painter.rect(ctx, 0, 0, canvas.offsetWidth, canvas.offsetHeight, "white");
-    painter.line(ctx, canvas.offsetWidth/2, center.y - activeHeight/2, canvas.offsetWidth/2, canvas.offsetHeight, 2, "lightgray");
-    painter.line(ctx, 0, center.y, canvas.offsetWidth, center.y, 2, "lightGray");
-    
-    //mouseposition
-    painter.circle(ctx, mouseState.lastRelativePosition.x + center.x, mouseState.lastRelativePosition.y + center.y, 5,"green", true);
-    painter.circle(ctx, mouseState.relativePosition.x + center.x, mouseState.relativePosition.y + center.y, 5,"red", true);
-    ctx.restore();
+    canvasState.ctx.save();
+    painter.clear(canvasState.ctx, 0, 0, canvasState.activeWidth, canvasState.totalHeight);
+    painter.rect(canvasState.ctx, 0, 0, canvasState.activeWidth, canvasState.totalHeight, "white");
+    painter.line(canvasState.ctx, canvasState.activeWidth/2, canvasState.center.y - canvasState.activeHeight/2, canvasState.activeWidth/2, canvasState.totalHeight, 2, "lightgray");
+    painter.line(canvasState.ctx, 0, canvasState.center.y, canvasState.activeWidth, canvasState.center.y, 2, "lightGray");
+    canvasState.ctx.restore();
 }
 
-module.exports = game;
+module.exports = Game;
